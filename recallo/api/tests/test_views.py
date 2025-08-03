5bce32ac7f21649c72c478da0a9d838654366831
linwangboi <linwangboi@gmail.com>
2025-08-03 13:55:26 +0000

    review session test done

--- recallo/api/tests/test_views.py
+++ recallo/api/tests/test_views.py
@@ -1,7 +1,7 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from api.models import StudyItem
+from api.models import StudyItem, ReviewSession
 from rest_framework import status
 from pprint import pprint
 
@@ -62,3 +62,67 @@ class StudyItemsViewsTest(APITestCase):
         response = self.client.get(reverse('user-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 3)
+
+
+class ReviewSessionViewsTest(APITestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.User = User  # Save class for reuse
+        self.staff = User.objects.create_user(
+            username='staff',
+            email='staff@staff.com',
+            password='staff',
+            is_staff=True
+        )
+        self.bob = User.objects.create_user(
+            username='bob',
+            email='bob@bob.com',
+            password='bob'
+        )
+        self.charlie = User.objects.create_user(
+            username='charlie',
+            email='charlie@charlie.com',
+            password='charlie'
+        )
+        # Study items for bob
+        self.bobitem1 = StudyItem.objects.create(
+            user=self.bob,
+            title='bobitem1',
+            description='bobdescription1'
+        )
+        self.bobreview1 = ReviewSession.objects.create(
+            user=self.bob,
+            study_item=self.bobitem1
+        )
+        self.bobitem2 = StudyItem.objects.create(
+            user=self.bob,
+            title='bobitem2',
+            description='bobdescription2'
+        )
+        self.bobreview2 = ReviewSession.objects.create(
+            user=self.bob,
+            study_item=self.bobitem2
+        )
+    def test_staff_can_see_all_study_items(self):
+        self.client.force_login(self.staff)
+        response = self.client.get(reverse('reviewsession-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_user_cannot_see_others_study_items(self):
+        self.client.force_login(self.charlie)
+        response = self.client.get(reverse('reviewsession-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_user_can_create_study_item(self):
+        self.client.force_login(self.bob)
+        response = self.client.post(reverse('reviewsession-list'), {
+            'user': self.bob.id,
+            'study_item': self.bobitem1.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['user'], self.bob.id)
+        self.assertEqual(response.data['study_item'], self.bobitem1.id)
+        self.assertEqual(len(ReviewSession.objects.filter(user=self.bob)), 3)
+        

80ae84b4ce6ae1045766789ce0f9c240ddc57b31
linwangboi <linwangboi@gmail.com>
2025-08-03 11:40:45 +0000

    review sessions factory done

--- recallo/api/factories.py
+++ recallo/api/factories.py
@@ -1,7 +1,7 @@
 import factory
 from factory.django import DjangoModelFactory
 
-from .models import StudyItem
+from .models import StudyItem, ReviewSession
 
 
 class StudyItemFactory(DjangoModelFactory):
@@ -14,3 +14,13 @@ class StudyItemFactory(DjangoModelFactory):
 
     title = factory.Faker("sentence", nb_words=4)
     description = factory.Faker("paragraph", nb_sentences=4)
+
+class ReviewSessionFactory(DjangoModelFactory):
+    class Meta:
+        model = ReviewSession
+    @factory.lazy_attribute
+    def user(self):
+        raise ValueError('You must provide a user')
+    @factory.lazy_attribute
+    def study_item(self):
+        raise ValueError('You must provide a study item')
--- recallo/api/management/commands/populate.py
+++ recallo/api/management/commands/populate.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
-from api.models import User
-from api.factories import StudyItemFactory
+from api.models import User, StudyItem
+from api.factories import StudyItemFactory, ReviewSessionFactory
 
 
 class Command(BaseCommand):
@@ -26,3 +26,10 @@ class Command(BaseCommand):
             StudyItemFactory(user=user1)
         for _ in range(10):
             StudyItemFactory(user=user2)
+        
+        for user in [user1, user2]:
+            for item in StudyItem.objects.filter(user=user):
+                for _ in range(5):
+                    ReviewSessionFactory(user=user, study_item=item)
+                    
+
